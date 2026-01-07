6e951f592df457b6d006c82f792dcbce691080d5
Muthukrishnan <m.g.muthukrishnan@accenture.com>
2026-01-07 14:59:35 +0530

    Fix project structure: remove nested directories

---
+++ trade-producer-service/src/test/java/com/trade/trade_producer_service/TradeProducerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trade.trade_producer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeProducerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
